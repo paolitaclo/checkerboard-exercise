eeea8b54e1bc1691c21cee8b14d6212aaf8e5e37
Paola Claros <paolita.claros@gmail.com>
2017-01-19 19:39:56 -0800

    homework checkerboard exercise done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Your JS goes here
+
+
+for (var i = 0; i < 81; i++) {
+  var div = document.createElement("div");
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  div.style.float = 'left';
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "black";
+  }else {
+    div.style.backgroundColor = "red";
+  }
+  document.body.append(div);
+}
